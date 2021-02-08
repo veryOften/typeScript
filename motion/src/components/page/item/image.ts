e1530accc13d6997d2df4776a3386a26e802864c
SangchoKim <wjdrms1919@gmail.com>
2021-02-08 23:46:48 +0900

    refactoring with baseComponent

--- motion/src/components/page/item/image.ts
+++ motion/src/components/page/item/image.ts
@@ -1,14 +1,12 @@
-export class ImageComponent {
-  private element: HTMLElement;
+import { BaseComponent } from "./../../component.js";
+export class ImageComponent extends BaseComponent<HTMLLIElement> {
   constructor(title: string, url: string) {
-    const template = document.createElement("template");
-    template.innerHTML = `<section class="image">
-    <div class="image__holder">
-        <img class="image__thumbnail">
-        <p class="image__title"></p>
-    </div>
-</section>`;
-    this.element = template.content.firstElementChild! as HTMLElement;
+    super(`<section class="image">
+            <div class="image__holder">
+              <img class="image__thumbnail">
+                <p class="image__title"></p>
+            </div>
+          </section>`);
 
     const imageElement = this.element.querySelector(
       ".image__thumbnail"
@@ -20,9 +18,5 @@ export class ImageComponent {
       ".image__title"
     )! as HTMLParagraphElement;
     titleElement.textContent = title;
-  }
-
-  attachTo(parent: HTMLElement, position: InsertPosition = "afterbegin") {
-    parent.insertAdjacentElement(position, this.element);
   }
 }
--- motion/src/components/page/page.ts
+++ motion/src/components/page/page.ts
@@ -1,12 +1,6 @@
-export class PageComponent {
-  private element: HTMLUListElement;
+import { BaseComponent } from "../component.js";
+export class PageComponent extends BaseComponent<HTMLUListElement> {
   constructor() {
-    this.element = document.createElement("ul");
-    this.element.setAttribute("class", "page");
-    this.element.textContent = "This is PageComponenet";
-  }
-
-  attachTo(parent: HTMLElement, position: InsertPosition = "afterbegin") {
-    parent.insertAdjacentElement(position, this.element);
+    super('<ul class="page">This is PageComponent!</ul>');
   }
 }
